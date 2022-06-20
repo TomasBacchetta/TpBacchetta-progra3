a0f85fba959798805b8c40574ba785fa373c5530
TomasBacchetta <78171375+TomasBacchetta@users.noreply.github.com>
2022-06-20 05:21:21 -0300

    20/06 sprint 3

---
+++ parte3/app/Controllers/LoginController.php
@@ -0,0 +1,73 @@
+<?php
+
+use \App\Models\empleado as empleado;
+use \App\Models\admin as admin;
+//use \App\Middleware\AutentificadorJWT as AutentificadorJWT;
+use GuzzleHttp\Psr7\Stream;
+
+class LoginController {
+    public function VerificarUsuario($request, $response, $args){
+        $parametros = $request->getParsedBody();
+
+        $nombre = $parametros['nombre'];
+        $clave = $parametros['clave'];
+
+
+        if (empleado::verificarEmpleado($nombre, $clave)){
+            $tokenNuevo = AutentificadorJWT::CrearToken(
+                empleado::where("nombre", $nombre)->value("puesto"), 
+                empleado::where("nombre", $nombre)->value("id")
+            );
+            $response->getBody()->write(json_encode(array("token"=>$tokenNuevo)));
+
+            return $response;
+        }
+        if (admin::verificarAdmin($nombre, $clave)){
+            $tokenNuevo = AutentificadorJWT::CrearToken("admin", 999);
+            $response->getBody()->write(json_encode(array("token"=>$tokenNuevo)));
+
+            return $response;
+        }
+        
+        $response->getBody()->write("Datos erróneos");
+        return $response->withStatus(403);
+        
+    }
+
+    public function VerificarCliente($request, $response, $args){
+        $parametros = $request->getParsedBody();
+
+        $mesa_id = $parametros['mesa_id'];
+        $pedido_id = $parametros['pedido_id'];
+
+        /*
+        if (empleado::verificarEmpleado($nombre, $clave)){
+            $tokenNuevo = AutentificadorJWT::CrearToken(
+                empleado::where("nombre", $nombre)->value("puesto"), 
+                empleado::where("nombre", $nombre)->value("id")
+            );
+            $response->getBody()->write(json_encode(array("token"=>$tokenNuevo)));
+
+            return $response;
+        }
+        if (admin::verificarAdmin($nombre, $clave)){
+            $tokenNuevo = AutentificadorJWT::CrearToken("admin", 999);
+            $response->getBody()->write(json_encode(array("token"=>$tokenNuevo)));
+
+            return $response;
+        }
+        
+        $response->getBody()->write("Datos erróneos");
+        return $response->withStatus(403);
+        */
+        $tokenNuevo = AutentificadorJWT_Clientes::CrearToken($mesa_id, $pedido_id);
+        $response->getBody()->write(json_encode(array("token"=>$tokenNuevo)));
+
+        return $response;
+
+        
+        
+    }
+}
+
+?>
---
+++ parte3/app/models/empleado.php
@@ -0,0 +1,66 @@
+<?php
+/*
+BACCHETTA, TOMÁS
+TP PROGRAMACION 3 "LA COMANDA"
+SPRINT 1
+ALTA
+VISUALIZACION
+BASE DE DATOS
+
+
+*/
+
+namespace App\Models;
+
+
+use \Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class empleado extends Model{
+
+    public $incrementing = true;
+    
+    protected $fillable = [
+        'nombre',
+        'dni',
+        'clave',
+        'puesto'
+    ];
+
+    use SoftDeletes;
+
+    public static function verificarEmpleado($nombre, $clave){
+        $empleado = empleado::where("nombre", "=", $nombre)->where("clave", "=", $clave)->first();
+
+        if (isset($empleado)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function existeEmpleado($nombre){
+        $empleado = empleado::where("nombre", "=", $nombre)->first();
+        if (isset($empleado)){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    public static function existeMozo($id){
+        $empleado = empleado::where("id", $id)->where("puesto", "Mozo")->first();
+        if (isset($empleado)){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+}
+
+
+
+?>
